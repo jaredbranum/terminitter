911383a09b26dcbfbdc22bcfe922bf550df3c7c1
Jared Branum <jared.branum@gmail.com>
2010-10-21 22:09:51 -0400

    view cleaned up, command line arguments added (not fully implemented)

--- src/view.py
+++ src/view.py
@@ -1,38 +1,39 @@
 # view module for terminitter
 
-from datetime import datetime
-import time
+from time import strptime, strftime
+from datetime import datetime, timedelta
+#from fabulous import fg256, bg256
+# TODO: switch view to use curses
 
 TWEET_COLOR = '\033[0m'
 USERNAME_COLOR = '\033[38;5;39m' # TODO: change to twitter logo color (or close)
 AT_SIGN_COLOR = '\033[1;37m'
 TIME_COLOR = '\033[0;37m'
+TIME_BRACKET_COLOR = '\033[1;30m'
+SEPARATOR_COLOR = '\033[38;5;172m'
 NO_COLOR = '\033[0m'
-# TODO: probably switch to use fabulous for colored text
 
-def print_tweet(tweet):
+def print_tweet(tweet, full_color=False):
   user = tweet['user']['screen_name']
   text = tweet['text']
   ttime = format_tweet_time(tweet['created_at'])
   #import pdb; pdb.set_trace()
-  if True: # 256 colors
-    print '['+TIME_COLOR+ttime+NO_COLOR+'] '+AT_SIGN_COLOR+'@'+NO_COLOR+USERNAME_COLOR+user+NO_COLOR+' > '+TWEET_COLOR+text+NO_COLOR
+  if full_color: # 256 colors
+    print (
+      TIME_BRACKET_COLOR+'['+NO_COLOR+
+      TIME_COLOR+ttime+NO_COLOR+
+      TIME_BRACKET_COLOR+'] '+NO_COLOR+
+      AT_SIGN_COLOR+'@'+NO_COLOR+
+      USERNAME_COLOR+user+NO_COLOR+
+      SEPARATOR_COLOR+' > '+NO_COLOR+
+      TWEET_COLOR+text+NO_COLOR
+    )
   else: # TODO: 16 colors
     print 'whatever'
     
 def format_tweet_time(tweettime):
-  dtime = time.strptime(tweettime, "%a %b %d %H:%M:%S +0000 %Y")
-  hr = dtime.tm_hour % 12
-  if hr == 0:
-    hr = '12'
-  elif hr < 10:
-    hr = '0' + str(hr)
-  else:
-    hr = str(hr)
-  
-  # TODO: same for minutes
-    
-  return hr + ':' + mn
-  
-  #ttime = str(dtime.tm_hour % 12) + ":" + str(dtime.tm_min)
-  
+  # TODO: detect timezone and dst rather than hardcode to EST
+  return (
+    datetime.strptime(tweettime, "%a %b %d %H:%M:%S +0000 %Y")
+    + timedelta(hours=8) # EST
+  ).strftime("%I:%M")
